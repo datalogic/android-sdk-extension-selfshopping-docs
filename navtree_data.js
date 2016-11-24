0ce4f491aa3681d4e8b5f7f00124af182d335e30
Guido Davide Dall’Olio <guido.dallolio@datalogic.com>
2016-11-24 16:33:21 +0100

    v1.1 release
    Signed-off-by: Guido Davide Dall’Olio <guido.dallolio@datalogic.com>

---
+++ navtree_data.js
@@ -0,0 +1,15 @@
+var NAVTREE_DATA =
+[ [ "com.datalogic.extension.selfshopping", "reference/com/datalogic/extension/selfshopping/package-summary.html", [ [ "Classes", null, [ [ "Version", "reference/com/datalogic/extension/selfshopping/Version.html", null, "1" ] ]
+, null ] ]
+, "1" ], [ "com.datalogic.extension.selfshopping.cradle", "reference/com/datalogic/extension/selfshopping/cradle/package-summary.html", [ [ "Interfaces", null, [ [ "Cradle", "reference/com/datalogic/extension/selfshopping/cradle/Cradle.html", null, "1" ] ]
+, null ], [ "Classes", null, [ [ "CradleManager", "reference/com/datalogic/extension/selfshopping/cradle/CradleManager.html", null, "1" ] ]
+, null ], [ "Enums", null, [ [ "CradleType", "reference/com/datalogic/extension/selfshopping/cradle/CradleType.html", null, "1" ] ]
+, null ] ]
+, "1" ], [ "com.datalogic.extension.selfshopping.cradle.joyatouch", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/package-summary.html", [ [ "Interfaces", null, [ [ "CradleJoyaTouch", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CradleJoyaTouch.html", null, "1" ] ]
+, null ], [ "Classes", null, [ [ "ConfigArea", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/ConfigArea.html", null, "1" ], [ "CustomArea", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CustomArea.html", null, "1" ], [ "GenericArea", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/GenericArea.html", null, "1" ], [ "StateInfo", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/StateInfo.html", null, "1" ] ]
+, null ], [ "Enums", null, [ [ "LedAction", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LedAction.html", null, "1" ], [ "LockAction", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LockAction.html", null, "1" ] ]
+, null ] ]
+, "1" ] ]
+
+;
+
---
+++ reference/lists.js
@@ -0,0 +1,17 @@
+var DATA = [
+      { id:0, label:"com.datalogic.extension.selfshopping", link:"reference/com/datalogic/extension/selfshopping/package-summary.html", type:"package", deprecated:"false" },
+      { id:1, label:"com.datalogic.extension.selfshopping.Version", link:"reference/com/datalogic/extension/selfshopping/Version.html", type:"class", deprecated:"false" },
+      { id:2, label:"com.datalogic.extension.selfshopping.cradle", link:"reference/com/datalogic/extension/selfshopping/cradle/package-summary.html", type:"package", deprecated:"false" },
+      { id:3, label:"com.datalogic.extension.selfshopping.cradle.Cradle", link:"reference/com/datalogic/extension/selfshopping/cradle/Cradle.html", type:"class", deprecated:"false" },
+      { id:4, label:"com.datalogic.extension.selfshopping.cradle.CradleManager", link:"reference/com/datalogic/extension/selfshopping/cradle/CradleManager.html", type:"class", deprecated:"false" },
+      { id:5, label:"com.datalogic.extension.selfshopping.cradle.CradleType", link:"reference/com/datalogic/extension/selfshopping/cradle/CradleType.html", type:"class", deprecated:"false" },
+      { id:6, label:"com.datalogic.extension.selfshopping.cradle.joyatouch", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/package-summary.html", type:"package", deprecated:"false" },
+      { id:7, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.ConfigArea", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/ConfigArea.html", type:"class", deprecated:"false" },
+      { id:8, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.CradleJoyaTouch", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CradleJoyaTouch.html", type:"class", deprecated:"false" },
+      { id:9, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.CustomArea", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CustomArea.html", type:"class", deprecated:"false" },
+      { id:10, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.GenericArea", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/GenericArea.html", type:"class", deprecated:"false" },
+      { id:11, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.LedAction", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LedAction.html", type:"class", deprecated:"false" },
+      { id:12, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.LockAction", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LockAction.html", type:"class", deprecated:"false" },
+      { id:13, label:"com.datalogic.extension.selfshopping.cradle.joyatouch.StateInfo", link:"reference/com/datalogic/extension/selfshopping/cradle/joyatouch/StateInfo.html", type:"class", deprecated:"false" }
+
+    ];
