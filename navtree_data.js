38c4d5ad3b99a11f314e6baef527fab234bac1e7
nbrunetto <nicholas.brunetto@datalogic.com>
2017-07-03 11:22:46 +0200

    v1.3 release

--- navtree_data.js
+++ navtree_data.js
@@ -1,13 +1,13 @@
 var NAVTREE_DATA =
 [ [ "com.datalogic.extension.selfshopping", "reference/com/datalogic/extension/selfshopping/package-summary.html", [ [ "Classes", null, [ [ "Version", "reference/com/datalogic/extension/selfshopping/Version.html", null, "1" ] ]
 , null ] ]
-, "1" ], [ "com.datalogic.extension.selfshopping.cradle", "reference/com/datalogic/extension/selfshopping/cradle/package-summary.html", [ [ "Interfaces", null, [ [ "Cradle", "reference/com/datalogic/extension/selfshopping/cradle/Cradle.html", null, "1" ], [ "CradleInsertionListener", "reference/com/datalogic/extension/selfshopping/cradle/CradleInsertionListener.html", null, "2" ] ]
+, "1" ], [ "com.datalogic.extension.selfshopping.cradle", "reference/com/datalogic/extension/selfshopping/cradle/package-summary.html", [ [ "Interfaces", null, [ [ "Cradle", "reference/com/datalogic/extension/selfshopping/cradle/Cradle.html", null, "1" ], [ "CradleInsertionListener", "reference/com/datalogic/extension/selfshopping/cradle/CradleInsertionListener.html", null, "2" ], [ "Trolley", "reference/com/datalogic/extension/selfshopping/cradle/Trolley.html", null, "3" ], [ "TrolleyInsertionListener", "reference/com/datalogic/extension/selfshopping/cradle/TrolleyInsertionListener.html", null, "3" ] ]
 , null ], [ "Classes", null, [ [ "CradleManager", "reference/com/datalogic/extension/selfshopping/cradle/CradleManager.html", null, "1" ] ]
-, null ], [ "Enums", null, [ [ "CradleType", "reference/com/datalogic/extension/selfshopping/cradle/CradleType.html", null, "1" ] ]
+, null ], [ "Enums", null, [ [ "Cradle.InsertionState", "reference/com/datalogic/extension/selfshopping/cradle/Cradle.InsertionState.html", null, "3" ], [ "CradleType", "reference/com/datalogic/extension/selfshopping/cradle/CradleType.html", null, "1" ] ]
 , null ] ]
 , "1" ], [ "com.datalogic.extension.selfshopping.cradle.joyatouch", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/package-summary.html", [ [ "Interfaces", null, [ [ "CradleJoyaTouch", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CradleJoyaTouch.html", null, "1" ] ]
 , null ], [ "Classes", null, [ [ "ConfigArea", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/ConfigArea.html", null, "1" ], [ "CustomArea", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CustomArea.html", null, "1" ], [ "GenericArea", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/GenericArea.html", null, "1" ], [ "StateInfo", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/StateInfo.html", null, "1" ] ]
-, null ], [ "Enums", null, [ [ "LedAction", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LedAction.html", null, "1" ], [ "LockAction", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LockAction.html", null, "1" ] ]
+, null ], [ "Enums", null, [ [ "CradleJoyaTouch.Type", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/CradleJoyaTouch.Type.html", null, "3" ], [ "LedAction", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LedAction.html", null, "1" ], [ "LockAction", "reference/com/datalogic/extension/selfshopping/cradle/joyatouch/LockAction.html", null, "1" ] ]
 , null ] ]
 , "1" ], [ "com.datalogic.extension.selfshopping.led", "reference/com/datalogic/extension/selfshopping/led/package-summary.html", [ [ "Interfaces", null, [ [ "DimmingLed", "reference/com/datalogic/extension/selfshopping/led/DimmingLed.html", null, "2" ], [ "Led", "reference/com/datalogic/extension/selfshopping/led/Led.html", null, "2" ] ]
 , null ], [ "Classes", null, [ [ "DimmingSequence", "reference/com/datalogic/extension/selfshopping/led/DimmingSequence.html", null, "2" ], [ "DimmingSequencePoint", "reference/com/datalogic/extension/selfshopping/led/DimmingSequencePoint.html", null, "2" ], [ "SelfshoppingLedManager", "reference/com/datalogic/extension/selfshopping/led/SelfshoppingLedManager.html", null, "2" ] ]
@@ -18,7 +18,9 @@ var NAVTREE_DATA =
 , null ] ]
 , "2" ], [ "com.datalogic.extension.selfshopping.led.event", "reference/com/datalogic/extension/selfshopping/led/event/package-summary.html", [ [ "Interfaces", null, [ [ "LedEvent", "reference/com/datalogic/extension/selfshopping/led/event/LedEvent.html", null, "2" ] ]
 , null ] ]
-, "2" ] ]
+, "2" ], [ "com.datalogic.extension.selfshopping.power", "reference/com/datalogic/extension/selfshopping/power/package-summary.html", [ [ "Classes", null, [ [ "ChargingProfile", "reference/com/datalogic/extension/selfshopping/power/ChargingProfile.html", null, "3" ], [ "ManufacturerInfo", "reference/com/datalogic/extension/selfshopping/power/ManufacturerInfo.html", null, "3" ], [ "SelfshoppingBatteryManager", "reference/com/datalogic/extension/selfshopping/power/SelfshoppingBatteryManager.html", null, "3" ] ]
+, null ] ]
+, "3" ] ]
 
 ;
 
